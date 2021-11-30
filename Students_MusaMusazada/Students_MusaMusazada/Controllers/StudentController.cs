3aabfbd315c208aa96ce5b15a59c21f71c31ffc4
Musa Musazada <musaam@code.edu.az>
2021-12-01 01:45:37 +0400

    Initial Commit

---
+++ Students_MusaMusazada/Students_MusaMusazada/Controllers/StudentController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Students_MusaMusazada.HelperClasses;
+using System.Collections.Generic;
+
+namespace Students_MusaMusazada.Controllers
+{
+    public class StudentController : Controller
+    {
+        List<cStudent> StudentList = new List<cStudent>() {
+            new cStudent("Musa", "Musazada", 49),
+            new cStudent("Aytac", "Rajabli", 21),
+            new cStudent("Iftixar ", "Qaraxanli", 20),
+            new cStudent("Ali", "Isaxanli", 20),
+            new cStudent("Ayxan", "Abdullayev", 21),
+            new cStudent("Tofiq", "Ahmedov", 20),
+            new cStudent("Vasif", "Unkown", 18),
+            new cStudent("Tabriz", "Hajiyev", 18),
+            new cStudent("Narmin", "Unknown", 22),
+            new cStudent("Gunay", "Khalilova", 18),
+            new cStudent("Vahid", "Efendiyev", 49)
+            };
+        public IActionResult Index(int? id)
+        {
+            //If exists, retrive the data!
+            if (id!=null && (id.Value < StudentList.Capacity && id.Value >=1)) {
+
+                cStudent student = StudentList[id.Value-1];
+                ViewData["name"] = student.name;
+                ViewData["surname"] = student.surname;
+                ViewData["age"] = student.age;
+
+                return View("Student");
+                //Comment the line above and uncomment below to retrive the JSON!
+                //return Json(new { Name= student.name, Surname= student.surname, Age=student.age});
+            }
+
+            //If Null or not such ID !
+            ViewData["text"] = "No Student With Such ID"!;
+            return View("NotFound");
+           
+
+        }
+    }
+}
---
+++ Students_MusaMusazada/Students_MusaMusazada/HelperClasses/cStudent.cs
@@ -0,0 +1,15 @@
+﻿namespace Students_MusaMusazada.HelperClasses
+{
+    public class cStudent
+    {
+        public string name;
+        public string surname;
+        public int age;
+        public cStudent(string NAME, string SURNAME, int AGE)
+        {
+            this.name = NAME;
+            this.surname = SURNAME; 
+            this.age = AGE;
+        }
+    }
+}
